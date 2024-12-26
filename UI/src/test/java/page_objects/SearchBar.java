58353447dc01c84d61b604a950662f98cf5adcb7
unknown <geshanpasindu1@gmail.com>
2024-12-26 20:23:43 +0530

    search bar test

---
+++ UI/src/test/java/page_objects/SearchBar.java
@@ -0,0 +1,42 @@
+package page_objects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utils.Config;
+
+public class SearchBar {
+    WebDriver driver;
+
+    // Locators for the search bar and search button
+    By searchBar = By.xpath("//*[@id=\"txtSearch\"]");
+    By searchButton = By.xpath("//*[@id=\"btnSearch\"]"); // Update with the actual locator for the search button
+
+    public SearchBar(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Method to enter a search term
+    public void enterSearchTerm(String term) {
+        driver.findElement(searchBar).clear();
+        driver.findElement(searchBar).sendKeys(term);
+    }
+
+    // Method to click the search button
+    public void clickSearchButton() {
+        driver.findElement(searchButton).click();
+    }
+
+    // Method to validate the URL of the search results page
+    public boolean isSearchResultsPageURLCorrect(String searchTerm) {
+        String expectedURL = Config.get_base_url("BASE_URL") +"product/"+ searchTerm + "?PS=" + searchTerm;
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("Current URL: " + currentURL);
+        System.out.println("Expected URL: " + expectedURL);
+        return currentURL.equals(expectedURL);
+    }
+
+    // Method to validate if search results are displayed
+    public boolean areSearchResultsDisplayed() {
+        return driver.findElements(By.cssSelector(".productListHeading")).size() > 0;
+    }
+}
---
+++ UI/src/test/java/step_definitions/SearchSteps.java
@@ -0,0 +1,51 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import page_objects.SearchBar;
+import utils.DriverFactory;
+import utils.Config;
+public class SearchSteps {
+    WebDriver driver;
+    SearchBar searchBar;
+
+    @Step("User is on the homepage")
+    @Given("the user is on the homepage")
+    public void the_user_is_on_the_homepage() {
+        driver = DriverFactory.get_driver();
+        driver.get(Config.get_base_url("BASE_URL")); // Replace with the actual homepage URL
+        searchBar = new SearchBar(driver);
+        Allure.step("Navigated to the homepage:"+ Config.get_base_url("BASE_URL"));
+    }
+
+    @Step("User searches for the term: {searchTerm}")
+    @When("the user searches for {string}")
+    public void the_user_searches_for(String searchTerm) {
+        searchBar.enterSearchTerm(searchTerm);
+        Allure.step("Entered search term: " + searchTerm);
+        searchBar.clickSearchButton();
+        Allure.step("Clicked on the search button");
+    }
+
+    @Step("Verify search results page for the term: {searchTerm}")
+    @Then("the search results page for {string} should be displayed")
+    public void the_search_results_page_for_should_be_displayed(String searchTerm) {
+        boolean isCorrectURL = searchBar.isSearchResultsPageURLCorrect(searchTerm);
+        Allure.step("Expected URL:"+Config.get_base_url("BASE_URL")+"/product/" + searchTerm.toLowerCase() + "?PS=" + searchTerm.toLowerCase());
+        Assert.assertTrue(isCorrectURL, "The URL of the search results page is incorrect for term: " + searchTerm);
+        Allure.step("Search results page URL is correct");
+    }
+
+    @Step("Verify search results are displayed for the term: {searchTerm}")
+    @Then("search results for {string} should be displayed")
+    public void search_results_for_should_be_displayed(String searchTerm) {
+        boolean areResultsDisplayed = searchBar.areSearchResultsDisplayed();
+        Assert.assertTrue(areResultsDisplayed, "Search results are not displayed for term: " + searchTerm);
+        Allure.step("Search results are displayed for term: " + searchTerm);
+    }
+}
