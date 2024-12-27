4bb767968b84625806a7d3109567f20334f418f9
Geshan Randeni <geshanpasindu1@gmail.com>
2024-12-28 00:55:08 +0530

    my acc dropdown test case

--- UI/src/test/java/test_runners/TestRunner.java
+++ UI/src/test/java/test_runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(
-        features= {"src/test/resources/features/update_profile.feature"},
+        features= {"src/test/resources/features/my_account.feature"},
         glue = {"step_definitions"},
         plugin = {
         "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
