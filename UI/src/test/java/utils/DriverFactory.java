0e2324098386bb9a801ff0013e217e2daade8958
Geshan Randeni <geshanpasindu1@gmail.com>
2025-01-02 09:48:29 +0530

    chromedriver path issue fix

--- UI/src/test/java/utils/DriverFactory.java
+++ UI/src/test/java/utils/DriverFactory.java
@@ -9,7 +9,21 @@ public class DriverFactory {
 
     public static WebDriver get_driver(){
         if(driver == null){
-            System.setProperty("webdriver.chrome.driver","src/test/resources/driver/chromedriver.exe");
+//            System.setProperty("webdriver.chrome.driver","src/test/resources/driver/chromedriver.exe");
+//            driver = new ChromeDriver();
+            
+            String os = System.getProperty("os.name").toLowerCase();
+            String driverPath = "src/test/resources/driver/chromedriver";
+
+            // Append .exe for Windows systems
+            if (os.contains("win")) {
+                driverPath += ".exe";
+            }
+
+            // Set the ChromeDriver system property dynamically
+            System.setProperty("webdriver.chrome.driver", driverPath);
+
+            // Initialize WebDriver
             driver = new ChromeDriver();
 //            driver.get('www.google.com') -> mekaa page modesls waladi initialize karaganna
 //            driver.manage().window().maximize();
