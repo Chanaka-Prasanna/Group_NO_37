25fbf47467dea13fa949068ff6f66c2b1988aec1
Chanaka-Prasanna <chanakapinfo@gmail.com>
2025-01-03 21:44:56 +0530

    modified runner class

--- UI/src/test/java/test_runners/TestRunner.java
+++ UI/src/test/java/test_runners/TestRunner.java
@@ -7,7 +7,7 @@ import org.testng.annotations.DataProvider;
 //@RunWith(Cucumber.class)
 
 @CucumberOptions(
-        features = "src/test/resources/features/login.feature",
+        features = "src/test/resources/features",
         glue = {"step_definitions"},
         monochrome = true,
         plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
